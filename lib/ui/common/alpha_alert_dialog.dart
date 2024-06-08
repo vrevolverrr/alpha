64376568b2c0ccb5b3aaa716aac3cbfe49ffc1e4
Bryan Soong <whiterevolver35@gmail.com>
2024-06-09 01:05:40 +0800

    :hammer: added docs

--- lib/ui/common/alpha_alert_dialog.dart
+++ lib/ui/common/alpha_alert_dialog.dart
@@ -1,97 +1,12 @@
 import 'package:alpha/extensions.dart';
 import 'package:alpha/ui/common/alpha_button.dart';
+import 'package:alpha/ui/common/should_render_widget.dart';
 import 'package:flutter/material.dart';
 
-class DialogButtonData {
-  /// Helper class to encapsulate the properties of the
-  final String title;
-  final double width;
-  final double height;
-  final void Function()? onTap;
-
-  static DialogButtonData cancel(BuildContext context) =>
-      DialogButtonData(title: "Cancel", onTap: () => context.dismissDialog());
-
-  static DialogButtonData confirm({void Function()? onTap}) =>
-      DialogButtonData(title: "Confirm", onTap: onTap);
-
-  const DialogButtonData(
-      {required this.title,
-      this.width = 280.0,
-      this.height = 60.0,
-      this.onTap});
-}
-
-class _AlertDialogContents extends StatelessWidget {
-  /// Creates a widget for the contents of the [AlphaAlertDialog].
-
-  /// The top title of the dialog.
-  final String title;
-
-  /// The properties of the "next" and cancel buttons of the dialog.
-  final DialogButtonData? next;
-  final DialogButtonData? cancel;
-
-  /// The child to display below the title.
-  final Widget child;
-
-  const _AlertDialogContents(
-      {required this.title, this.next, this.cancel, required this.child});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Text(
-          title.toUpperCase(),
-          style: const TextStyle(
-              fontFamily: "LexendMega",
-              fontWeight: FontWeight.w700,
-              fontSize: 34.0),
-        ),
-        const SizedBox(height: 10.0),
-        Container(
-          height: 3.0,
-          width: 300.0,
-          decoration: BoxDecoration(
-              color: Colors.black45, borderRadius: BorderRadius.circular(10.0)),
-        ),
-        const SizedBox(height: 20.0),
-        child,
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            cancel != null
-                ? AlphaButton(
-                    width: cancel!.width,
-                    height: cancel!.height,
-                    title: cancel!.title,
-                    onTap: cancel!.onTap,
-                  )
-                : const SizedBox(),
-            (next != null && cancel != null)
-                ? const SizedBox(width: 20.0)
-                : const SizedBox(),
-            next != null
-                ? AlphaButton(
-                    width: next!.width,
-                    height: next!.height,
-                    title: next!.title,
-                    color: const Color.fromARGB(255, 164, 211, 151),
-                    onTap: next!.onTap,
-                  )
-                : const SizedBox()
-          ],
-        )
-      ],
-    );
-  }
-}
-
+/// Creates a custom alert dialog widget in the style of the game.\
+/// This widget should only be used by [AlphaScaffold] and not be
+/// created anywhere else.
 class AlphaAlertDialog extends StatefulWidget {
-  /// The main widget that defines the custom alert dialog
-
   /// The top title of the dialog.
   final String title;
 
@@ -125,7 +40,8 @@ class _AlphaAlertDialogState extends State<AlphaAlertDialog> {
           widget.show ? const Duration(milliseconds: 800) : Durations.short4,
       curve: widget.show ? Curves.elasticOut : Curves.decelerate,
 
-      /// animates the dialog expanding and closing
+      /// Animates the dialog expanding and closing, when the
+      /// widget's `show` property changes
       width: widget.show ? 650.0 : 0.0,
       height: widget.show ? 380.0 : 0.0,
       padding: const EdgeInsets.all(20.0),
@@ -137,18 +53,108 @@ class _AlphaAlertDialogState extends State<AlphaAlertDialog> {
             BoxShadow(color: Colors.black, offset: Offset(1.0, 5.0))
           ]),
 
-      /// scale the contents of the dialog as it expands and closes
-      child: widget.show
-          ? FittedBox(
-              fit: BoxFit.contain,
-              child: _AlertDialogContents(
-                title: widget.title,
-                next: widget.next,
-                cancel: widget.cancel,
-                child: widget.child,
-              ),
-            )
-          : null,
+      /// Scale the contents of the dialog as it expands and closes.
+      child: RenderIfTrue(
+        condition: widget.show,
+        child: FittedBox(
+          fit: BoxFit.contain,
+          child: _AlertDialogContents(
+            title: widget.title,
+            next: widget.next,
+            cancel: widget.cancel,
+            child: widget.child,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/// Helper class to encapsulate the parameters of the buttons of [AlphaAlertDialog].
+class DialogButtonData {
+  final String title;
+  final double width;
+  final double height;
+  final void Function()? onTap;
+
+  const DialogButtonData(
+      {required this.title,
+      this.width = 280.0,
+      this.height = 60.0,
+      this.onTap});
+
+  /// Creates a [DialogButtonData] for a default `Cancel` button.
+  static DialogButtonData cancel(BuildContext context) =>
+      DialogButtonData(title: "Cancel", onTap: () => context.dismissDialog());
+
+  /// Creates a [DialogButtonData] for a default `Confirm` button
+  static DialogButtonData confirm({void Function()? onTap}) =>
+      DialogButtonData(title: "Confirm", onTap: onTap);
+}
+
+/// Creates a widget for the contents of the [AlphaAlertDialog].
+class _AlertDialogContents extends StatelessWidget {
+  /// The top title of the alert dialog.
+  final String title;
+
+  /// The data of the `next` button of the alert dialog.
+  final DialogButtonData? next;
+
+  /// The data of the `cancel` button of the alert dialog.
+  final DialogButtonData? cancel;
+
+  /// The `child` to display below the title.
+  final Widget child;
+
+  const _AlertDialogContents(
+      {required this.title, this.next, this.cancel, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text(
+          title.toUpperCase(),
+          style: const TextStyle(
+              fontFamily: "LexendMega",
+              fontWeight: FontWeight.w700,
+              fontSize: 34.0),
+        ),
+        const SizedBox(height: 10.0),
+        Container(
+          height: 3.0,
+          width: 300.0,
+          decoration: BoxDecoration(
+              color: Colors.black45, borderRadius: BorderRadius.circular(10.0)),
+        ),
+        const SizedBox(height: 20.0),
+        child,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            RenderIfNotNull(
+                nullable: cancel,
+                child: AlphaButton(
+                  width: cancel!.width,
+                  height: cancel!.height,
+                  title: cancel!.title,
+                  onTap: cancel!.onTap,
+                )),
+            RenderIfAllNotNull(
+                nullables: [next, cancel], child: const SizedBox(width: 20.0)),
+            RenderIfNotNull(
+                nullable: next,
+                child: AlphaButton(
+                  width: next!.width,
+                  height: next!.height,
+                  title: next!.title,
+                  color: const Color.fromARGB(255, 164, 211, 151),
+                  onTap: next!.onTap,
+                ))
+          ],
+        )
+      ],
     );
   }
 }
--- lib/ui/common/should_render_widget.dart
+++ lib/ui/common/should_render_widget.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
+/// Creates a widget that conditionally render its `child` when
+/// the `nullable` value provided is null.
+class RenderIfNull<T> extends StatelessWidget {
+  final T nullable;
+  final Widget child;
+
+  const RenderIfNull({super.key, required this.nullable, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return (nullable == null) ? child : const SizedBox();
+  }
+}
+
+/// Creates a widget that conditionally render its `child` when
+/// the `nullable` value provided is not null.
 class RenderIfNotNull<T> extends StatelessWidget {
   final T nullable;
   final Widget child;
@@ -13,6 +29,27 @@ class RenderIfNotNull<T> extends StatelessWidget {
   }
 }
 
+/// Creates a widget that conditionally render its `child` when
+/// all the values in `nullables` provided are not null.
+class RenderIfAllNotNull<T> extends StatelessWidget {
+  final List<T> nullables;
+  final Widget child;
+
+  const RenderIfAllNotNull(
+      {super.key, required this.nullables, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    for (final nullable in nullables) {
+      if (nullable == null) return const SizedBox();
+    }
+
+    return child;
+  }
+}
+
+/// Creates a widget that conditionally render its `child` when
+/// the `condition` provided is true.
 class RenderIfTrue extends StatelessWidget {
   final bool condition;
   final Widget child;
@@ -22,6 +59,8 @@ class RenderIfTrue extends StatelessWidget {
   Widget build(BuildContext context) => condition ? child : const SizedBox();
 }
 
+/// Creates a widget that conditionally render its child when
+/// the `condition` provided is false.
 class RenderIfFalse extends StatelessWidget {
   final bool condition;
   final Widget child;
