6064bfb44957fa9c91688a5c077525d9f5d462c8
Bryan Soong <whiterevolver35@gmail.com>
2024-06-17 22:14:33 +0800

    refactor: modified all text styles to use static const value

--- lib/ui/screens/dice_roll/dice_roll_screen.dart
+++ lib/ui/screens/dice_roll/dice_roll_screen.dart
@@ -1,5 +1,4 @@
 import 'package:alpha/extensions.dart';
-import 'package:alpha/main.dart';
 import 'package:alpha/services.dart';
 import 'package:alpha/ui/common/alpha_alert_dialog.dart';
 import 'package:alpha/ui/common/alpha_button.dart';
