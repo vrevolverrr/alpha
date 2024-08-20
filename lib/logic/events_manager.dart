1cb96db19d7239a6764e1cc0b5e8a2efa2c12e44
Bryan Soong <whiterevolver35@gmail.com>
2024-08-20 17:02:10 +0800

    :fire: feat: events manager

--- lib/logic/events_manager.dart
+++ lib/logic/events_manager.dart
@@ -1,6 +1,17 @@
-abstract class AlphaEvent {}
+import 'package:alpha/logic/players_logic.dart';
+import 'package:alpha/services.dart';
+import 'package:flutter/widgets.dart';
 
-class AlphaPlayerEvent implements AlphaEvent {}
+typedef AlphaAction = void Function(Player activePlayer);
+
+class AlphaEvent {
+  VoidCallback action;
+  Player target;
+
+  bool shouldTrigger() => activePlayer == target;
+
+  AlphaEvent({required this.target, required this.action});
+}
 
 class AlphaEventsManager {
   void queue(AlphaEvent event) {}
