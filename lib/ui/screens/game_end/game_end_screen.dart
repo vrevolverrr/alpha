0cd33460355850d70e97dc32902597c2f5a4eac0
Bryan Soong <whiterevolver35@gmail.com>
2025-01-22 01:59:06 +0800

    fix: minor adjustments in game end screen

--- lib/ui/screens/game_end/game_end_screen.dart
+++ lib/ui/screens/game_end/game_end_screen.dart
@@ -27,8 +27,8 @@ class GameEndScreen extends StatelessWidget {
         Text("Rounds Played: ${gameManager.round}", style: TextStyles.bold22),
         const SizedBox(height: 5.0),
         Text(
-            "Total Points: ${gameManager.leaderboard.fold(0.0, (value, ld) => value + ld.points)}",
-            style: TextStyles.bold24),
+            "Total Points: ${gameManager.leaderboard.fold(0.0, (value, ld) => value + ld.points).toInt()}",
+            style: TextStyles.bold28),
         const SizedBox(height: 5.0),
         const Text("Leaderboard", style: TextStyles.bold32),
         const SizedBox(height: 5.0),
@@ -36,7 +36,7 @@ class GameEndScreen extends StatelessWidget {
           padding: const EdgeInsets.only(left: 20.0),
           child: SizedBox(
             width: 540.0,
-            height: MediaQuery.sizeOf(context).height - 230.0,
+            height: MediaQuery.sizeOf(context).height - 270.0,
             child: AlphaScrollbar(
               controller: _controller,
               child: SingleChildScrollView(
